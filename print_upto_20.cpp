dbf007b706065877d8035046a822cadbc99ccf13
akarshhkh <akarshkhot@gmail.com>
2024-09-26 12:23:45 +0530

    Create print_upto_20.cpp

---
+++ print_upto_20.cpp
@@ -0,0 +1,39 @@
+//akarsh khot
+//23070123012
+//Aim - to print upto 20 using while loops.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i=1;
+    while(i<=20){
+        cout<<i << endl;
+        i++;
+            
+    }
+    return 0;
+}
+/*
+OUTPUT 
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+*/
