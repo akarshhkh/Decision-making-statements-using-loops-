9373b63569732478139cd2854941161b7e3d2bf1
akarshhkh <akarshkhot@gmail.com>
2024-09-26 12:22:25 +0530

    Create hello_world_using_loops.cpp

---
+++ hello_world_using_loops.cpp
@@ -0,0 +1,24 @@
+//akarsh khot
+//23070123012
+//Aim - print hello world using for loop for n number of times.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i;
+    for (i = 1; i <= 5; i++)
+    {
+        cout << "hello world" << endl;
+    }
+    return 0;
+}
+
+/* 
+OUTPUT
+hello world
+hello world
+hello world
+hello world
+hello world
+*/
